10647e3c39646fb91dceb3d2ec6a580f4e3ee080
Jonas Vitorio <jonasvitorio35@gmail.com>
2024-11-19 11:24:20 -0300

    Update the exercise in JS

--- javaScript/comentarios.js
+++ javaScript/comentarios.js
@@ -7,15 +7,27 @@ estou mostrando como faz comentário
 
 //programa que calcula quanto será gasto numa viagem
 
-const combustivel = 5.75; // valor do combustível
+let combustivel // valor do combustível
 let distancia = 10; // valor da distância em KM da viagem
 let gastoMedio = 2048; // gasto médio de combustível por KM
 const resultado = gastoMedio / distancia
+let tipocombustivel = "etanol"
 
-const medio = resultado * combustivel
-//gastoMedio = distancia / combustivel
 
-console.log( "O valor gasto nessa viagem será de  R$ " + medio.toFixed(2) + " combustível do carro por KM ");
+
+if( tipocombustivel == "gasolina") { // dependendo do tipo de combustível, muda o valor da viagem
+    combustivel = 6.10
+    const medio = resultado * combustivel
+    console.log( "O valor gasto nessa viagem será de  R$ " + medio.toFixed(2) + " combustível do carro por KM ");
+} else if( tipocombustivel == "etanol") {
+    combustivel = 4.04
+    const medio = resultado * combustivel
+    console.log( "O valor gasto nessa viagem será de  R$ " + medio.toFixed(2) + " combustível do carro por KM ");
+} else {
+    console.log("valor inválido")
+}
+
+
 
 
 
