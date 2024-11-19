4a502981ff8d74bc5c249eaca0f8a9f22453c0b1
Jonas Vitorio <jonasvitorio35@gmail.com>
2024-11-19 00:05:43 -0300

    New exercise in JS

--- javaScript/comentarios.js
+++ javaScript/comentarios.js
@@ -5,8 +5,17 @@
 estou mostrando como faz comentário
 */
 
+//programa que calcula quanto será gasto numa viagem
 
-let hush = "Jonas"
- console.log(hush)
+const combustivel = 5.75; // valor do combustível
+let distancia = 10; // valor da distância em KM da viagem
+let gastoMedio = 2048; // gasto médio de combustível por KM
+const resultado = gastoMedio / distancia
+
+const medio = resultado * combustivel
+//gastoMedio = distancia / combustivel
+
+console.log( "O valor gasto nessa viagem será de  R$ " + medio.toFixed(2) + " combustível do carro por KM ");
+
 
 
