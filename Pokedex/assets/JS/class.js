c7ea7e428c1979fa184bc742a9a581a694a5210a
Jonas Vitorio <jonasvitorio35@gmail.com>
2024-12-08 21:41:15 -0300

    putting documentation

--- Pokedex/assets/JS/class.js
+++ Pokedex/assets/JS/class.js
@@ -4,6 +4,9 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
 
-fetch(url).then( function (response) {
-        console.log(response)
-})
+fetch(url)
+    .then( (response)  => response.json()) //then is used to when reponse is true
+    .then( (jsonBody) => console.log(jsonBody)) //show the body of the document
+    .catch( (error)=> console.log(error)) // catch is used to if have a problem
+
+    
