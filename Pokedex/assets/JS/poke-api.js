f5d18829ca8feca2c91b463ee82ddec731fcc8a0
Jonas Vitorio <jonasvitorio35@gmail.com>
2024-12-11 11:31:20 -0300

    Putting color in the pokemons

--- Pokedex/assets/JS/poke-api.js
+++ Pokedex/assets/JS/poke-api.js
@@ -23,7 +23,7 @@ pokeApi.getPokemonDetail = (pokemon) => { // Show the details of pokemons for js
            .then(convertPokeApiDetailToPokemon)
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
     
     return fetch(url)
