11efa23062c1359d1841b0dc91cbf5c6ae061ac7
Jonas Vitorio <jonasvitorio35@gmail.com>
2025-01-23 23:59:57 -0300

    Adding audio to piano

--- Piano/src/scripts/order.js
+++ Piano/src/scripts/order.js
@@ -1,5 +1,13 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .keys");
 
+let audio = new  Audio("src/tunes/a.wav");
+
+const playTune = (key) => {
+    audio.src = `src/tunes/${key}.wav`; /* play each piano key */
+    audio.play();
+};
+
 pianoKeys.forEach((key) => {
-    
+    console.log(key.dataset.key);
+    key.addEventListener("click", () => playTune(key.dataset.key));
 })
