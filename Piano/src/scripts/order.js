ca2a645f570ac2c7044a5040fdc996032862bfb1
Jonas Vitorio <jonasvitorio35@gmail.com>
2025-01-25 21:40:27 -0300

    I adding volume and toggle

--- Piano/src/scripts/order.js
+++ Piano/src/scripts/order.js
@@ -1,5 +1,7 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .keys");
 
+const keysCheck = document.querySelector(".keys-check input");
+const volumeSlider = document.querySelector(".volume-slider input");
 let mapedKeys = [];
 let audio = new  Audio("src/tunes/a.wav");
 
@@ -21,9 +23,21 @@ pianoKeys.forEach((key) => {
 
 document.addEventListener("keydown", 
     (e) => {
-
         if (mapedKeys.includes(e.key)) {
             playTune(e.key);
         }
        // When I click on the keybord, the song out
     });
+
+    const handleVolume = (e) => {
+        audio.volume = e.target.value;
+  
+    };
+
+    const showHideKeys = () => {
+     pianoKeys.forEach(key => key.classList.toggle("hide"))
+    }
+
+volumeSlider.addEventListener("input", handleVolume);
+
+keysCheck.addEventListener("click", showHideKeys)
