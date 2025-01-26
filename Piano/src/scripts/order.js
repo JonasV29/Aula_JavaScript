d98c11f6c9a2de0951ae0ad243802deb18c9bb70
Jonas Vitorio <jonasvitorio35@gmail.com>
2025-01-25 21:00:16 -0300

    Update the code

--- Piano/src/scripts/order.js
+++ Piano/src/scripts/order.js
@@ -5,9 +5,20 @@ let audio = new  Audio("src/tunes/a.wav");
 const playTune = (key) => {
     audio.src = `src/tunes/${key}.wav`; /* play each piano key */
     audio.play();
+
+const clickedkey = document.querySelector(`[data-key="${key}"]`);
+    clickedkey.classList.add("active");
+    setTimeout(() => {
+        clickedkey.classList.remove("active");
+    }, 150);
 };
 
 pianoKeys.forEach((key) => {
     console.log(key.dataset.key);
     key.addEventListener("click", () => playTune(key.dataset.key));
-})
+}); // Whem I click on keys, play the song
+
+document.addEventListener("keydown", 
+    (e) => {
+        playTune(e.key); // When I click on the keybord, the song out
+    });
